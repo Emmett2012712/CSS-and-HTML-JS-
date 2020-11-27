4c199962cb6516204f31c290e88aefd695d166fc
Emmett The Coder <75128152+Emmett2012712@users.noreply.github.com>
2020-11-27 11:22:04 -0500

    Create script2.js
    A function that writes code a function and you are cool.

---
+++ script2.js
@@ -0,0 +1,7 @@
+// in this one we're learning about Functions. //
+function myFunction() {
+console.log('Code a function and you are cool.')
+}
+// if you are putting variables inside of your code put it in the parentheses. Nothing will rn when you pt that console.log. You have to put myFunction(); For it to work.//
+myFunction();
+// You can learn what a document.getElementById is in the next one! //
