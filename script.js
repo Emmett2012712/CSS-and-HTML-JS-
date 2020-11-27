5f480a22eb422b58f59227d4148dbcecba7911d7
Emmett The Coder <75128152+Emmett2012712@users.noreply.github.com>
2020-11-27 11:13:35 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// This is a comment in js. //
+/* This is a 
+multi line comment in js. */
+// Var is a variable in JS. //
+var a = 7
+var myName = "Emmett"
+// console.log(myName); If you write that it will print in the console "Emmett". //
+console.log(myName);
